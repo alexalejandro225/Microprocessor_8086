18cc12052df163309be3889bf976186412748924
alejandro <alejandroaguilarvea@gmail.com>
2020-03-10 23:48:26 -0700

    .

--- Ram_Test/practica.c
+++ Ram_Test/practica.c
@@ -1,4 +1,3 @@
-#define ram_offset 0x2200
 extern unsigned char peek(unsigned int offset);
 extern void poke(unsigned int offset, unsigned char data) ;
 extern void myputchar(char x);
@@ -6,105 +5,112 @@ extern char getch();
 void myItoa(unsigned int num,int base, char *salida);
 void puts(char *str);
 char rotChar (char x, char n);
+void test_ceros();
+void test_ones();
 char prueba2[50];
 char symbol[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 int i,j,t,contador=0;
-char contador_entrada=0;
-char salida[3];
+unsigned char contador_entrada=0;
+unsigned char salida[5];
 
 
-int main(void)
-{ int i,offset=1,test; 
+int main()
+{
+    int i;
+    
     while(1)
     {
-        puts("CARGANDO DATOS\n\r");
+        puts("Presiona enter para cargar ceros\n\r");
         getch();
-        for(i=0; i<8; i++)                  /*loading walking ones to the memory*/
+        for(i=0; i<0x1000; i++)
         {
-            poke(ram_offset+offset,offset);
-            offset=offset<<1;
+            poke(i,1);
         }
-        offset=1;
 
-        puts("PRESIONA ENTER PARA PROBAR LINEAS DE DATOS\n\r");
+        puts("Presiona enter para probar ceros\n\r");
         getch();
-        for(i=0; i<8; i++)
-        {
-            myItoa(i,10,salida);
+        
+        
 
-            if(peek(ram_offset+offset)==offset)         /*test walking ones on the memory*/
-            {
-                puts("Linea de datos correcta_");
-                puts(salida);
-                puts("\n\r");
-            }
-            else 
-            {
-                puts("FALLA CRITICA LINEA DE DATOS/DIRECCIONES_");
-                puts(salida);
-                puts("\n\r");
-            }
-            offset=offset<<1;
-        }
-        /*===============================================================*/
-        puts("CARGANDO DATOS DE LINEA DE DIRECCIONES\n\r");
-        getch();
-        for(i=8; i<13; i++)
-        {
-            if(i%2)
-            {
-                poke(ram_offset+offset,0xAA);           /*loading test data 0x55 and 0xAA pattern to memory*/
-            }
-
-            else
-            {
-                poke(ram_offset+offset,0x55);
-            }
-         offset=offset<<1;
-        }
-        offset=256;
-
-        puts("PRESIONA ENTER PARA PROBAR LINEAS DE DIRECCION\n\r");         /* test data 0x55 and 0xAA pattern on memory*/
-        getch();
-        for(i=8; i<13; i++)
-        {
-            myItoa(i,10,salida);
-            if(i%2)
-            {
-                if(peek(ram_offset+offset)==0xAA)
-                {
-                    puts("Linea de direccion correcta_");
-                    puts(salida);
-                    puts("\n\r");
-                }
-                else
-                {
-                    puts("Linea de direccion incorrecta_");
-                    puts(salida);
-                    puts("\n\r");
-                }
-                
-            }
-            else
-            {
-                if(peek(ram_offset+offset)==0x55)
-                {
-                    puts("Linea de direccion correcta_");
-                    puts(salida);
-                    puts("\n\r");
-                }
-                else
-                {
-                    puts("Linea de direccion incorrecta_");
-                    puts(salida);
-                    puts("\n\r");
-                }
-            }
-         offset=offset<<1;
-        }
-        offset=1;
+        
     }
-    return 0;
+}
+
+void test_ones()
+{
+    unsigned int i=0;
+    unsigned char data_out;
+
+        i=0;
+        
+        while(i<0x1000)
+        {
+            poke(i,1);
+            i++;
+        }
+
+        i=0;
+
+         while(i<0x1000) 
+        {
+            data_out=peek(i);
+            myItoa(data_out,16,salida);
+
+            if(data_out==1)
+            {
+                puts("CELDA DE MEMORIA CORRECTA NUM_");
+                puts(salida);
+                puts("\n\r");
+            }
+
+            else
+            {
+                puts("CELDA DE MEMORIA FALLIDA NUM_");
+                puts(salida);
+                puts("\n\r");
+            }
+            i++;
+        }
+}
+
+void test_ceros()
+{
+    unsigned int i=0;
+    unsigned char data_out;
+
+    puts("PRESIONA ENTER PRUEBA EN CEROS\n\r");
+        getch();
+
+        i=0;
+        
+        while(i<0x1000)
+        {
+            poke(i,0);
+            i++;
+        }
+
+        i=0;
+
+         while(i<0x1000) 
+        {
+            data_out=peek(i);
+            myItoa(data_out,16,salida);
+
+            if(data_out==0)
+            {
+                puts("CELDA DE MEMORIA CORRECTA NUM_");
+                puts(salida);
+                puts("\n\r");
+            }
+
+            else
+            {
+                puts("CELDA DE MEMORIA FALLIDA NUM_");
+                puts(salida);
+                puts("\n\r");
+            }
+            i++;
+        }
 }
 
 void myItoa(unsigned int num,int base, char *salida)
--- Ram_Test/putchar.asm
+++ Ram_Test/putchar.asm
@@ -10,9 +10,12 @@
     _peek  PROC
                 push bp
                 mov bp,sp
+                mov cx,ds
+                mov ax,220h
+                mov ds,ax
                 mov bx,[bp+4] ;Ram offset 1 word size
-                
                 mov al,[bx]   ;return data from pointer
+                mov ds,cx
                 pop bp
                 ret
     _peek ENDP
@@ -20,10 +23,13 @@
     _poke	proc
             push bp
             mov bp,sp
-            
+            mov cx,ds
             mov bx,[bp+4]   ;Ram offset 1 word size
+            mov ax,220h
+            mov ds,ax
             mov al,[bp+6]   ;get byte of data from the stack
             mov byte ptr [bx],al ;move data to memory 
+            mov ds,cx
             pop bp			
 			ret
 	_poke	ENDP
@@ -31,7 +37,6 @@
     _myputchar  PROC
                 push bp
                 mov bp,sp
-
                 mov dl,[bp+4]
                 mov ah,2
                 int 21h
