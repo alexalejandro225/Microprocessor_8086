2158681c99048725e4406a165abc97cd9d8bacdc
alejandro <alejandroaguilarvea@gmail.com>
2020-03-26 22:26:24 -0700

    .

--- 8255_config/practica.c
+++ 8255_config/practica.c
@@ -6,15 +6,22 @@
 #define RCtr 0x43
 #define PTOs_all_out 0x80
 #define PTOs_all_in 0x9B
+#define click_delay 10
 extern void myputchar(char x);
 extern char getch(); 
 extern void outportb( WORD port, BYTE dato);
 extern BYTE inportb( WORD port);
+
 void SetBitPort(WORD Puerto, BYTE num_bit);
 BYTE TstBitPort ( WORD Puerto, BYTE num_bit );
+void ClrBitPort(WORD Puerto,BYTE num_bit);
+void NotBitPort(WORD Puerto,BYTE num_bit);
+void ReversBitsPort(WORD Puerto);
 void myItoa(unsigned int num,int base, char *salida);
 void puts(char *str);
-char rotChar (char x, char n);
+unsigned char reverseBits(unsigned char n);
+unsigned char rotChar (unsigned char x,unsigned char n);
+void delay(BYTE counter_cycles);
 void led1_out();
 void led2_out();
 void led3_out();
@@ -24,42 +31,107 @@ void led6_out();
 char prueba2[50];
 char symbol[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 unsigned char salida[10];
+BYTE port_bit_in[8]={0};
 int j,i;
 
 
 int main()
 {   
-    int k;
-    int temp_dato;
+    WORD k;
+    BYTE temp_dato,data;
+
 
     outportb(RCtr,PTOs_all_in);
     
     while(1)
     {
+        data=0xAF;
+        temp_dato=0;
+        k=0;
+        puts("captura los bits de entrada\n\r");
         for(k=0;k<8;k++)
         {
             getch();
-            temp_dato+=TstBitPort(PC,4);
             temp_dato=temp_dato<<1;
+            temp_dato+=TstBitPort(PC,4);
             puts("DATO_IN\n\r");
         }
 
         myItoa(temp_dato,2,salida);
         puts(salida);
-        led1_out();
-        getch();
-        led2_out();
-        getch();
-        led3_out();
-        getch();
-        led4_out();
-        getch();
-        led5_out();
-        getch();
-        led6_out();
-        getch();
+        puts("\n\r");
+
+        temp_dato=reverseBits(temp_dato);
+
+        temp_dato=temp_dato>>2;
+
+        myItoa(temp_dato,2,salida);
+        puts(salida);
+
+
+        /*=======================================*/
+
+        for(k=0;k<8;k++)
+        {
+            port_bit_in[k]=temp_dato & 0x01;
+            temp_dato=temp_dato >> 1;
+        }
+
+
+
+
+        while(1)
+        {
+            if(port_bit_in[0]==1)
+            {
+                led1_out();
+                delay(click_delay);
+            }
+
+            if(port_bit_in[1]==1)
+            {
+                led2_out();
+                delay(click_delay);
+            }
+
+            if(port_bit_in[2]==1)
+            {
+                led3_out();
+                delay(click_delay);
+            }
+
+            if(port_bit_in[3]==1)
+            {
+                led4_out();
+                delay(click_delay);
+            }
+
+            if(port_bit_in[4]==1)
+            {
+                led5_out();
+                delay(click_delay);
+            }
+
+            if(port_bit_in[5]==1)
+            {
+                led6_out();
+                delay(click_delay);
+            }
+
+
+        }
+
     }
     return 0;
+}
+
+void delay(BYTE counter_cycles)
+{
+    BYTE steps=0;
+    for(steps; steps<counter_cycles; steps++)
+    {
+        asm nop;
+    }
 }
 
 void led1_out()
@@ -130,6 +202,55 @@ BYTE TstBitPort ( WORD Puerto,BYTE num_bit )
     return(0);
 }
 
+void ClrBitPort(WORD Puerto,BYTE num_bit)
+{
+ BYTE mask=0xFE; 
+ BYTE temp;
+ temp = inportb(Puerto); 
+ mask = rotChar(mask,num_bit);
+ temp = temp & mask;
+ outportb(Puerto,temp);
+}
+
+void NotBitPort(WORD Puerto,BYTE num_bit)
+{
+ BYTE mask=0x1; 
+ BYTE temp;
+ temp = inportb(Puerto); 
+ mask = mask << num_bit;
+ temp = temp ^ mask;
+ outportb(Puerto,temp);
+}
+
+void ReversBitsPort(WORD Puerto)
+{
+ BYTE temp;
+ temp = inportb(Puerto); 
+ temp = reverseBits(temp);
+ outportb(Puerto,temp);
+}
+
+unsigned char reverseBits(unsigned char n) 
+{ 
+    unsigned int rev = 0; 
+    
+    while (n > 0) 
+    { 
+       
+        rev <<= 1; 
+          
+        
+        if (n & 1 == 1) 
+        {
+            rev ^= 1; 
+        } 
+
+        n >>= 1; 
+              
+    } 
+    return rev; 
+} 
+
 
 void myItoa(unsigned int num,int base, char *salida)
 {
@@ -171,8 +292,8 @@ void puts(char* str)
     }
 }
 
-char rotChar (char x, char n)
+unsigned char rotChar (unsigned char x,unsigned char n)
 {
-  return (x<<n) | (x>>(8-n));
+  return (unsigned)(x<<n) | (x>>(8-n));
 }
 
