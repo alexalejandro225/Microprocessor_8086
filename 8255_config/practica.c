ba518263bff426a395978e5b70c5a31af1b6e247
ALEJANDRO <alejandroaguilarvea@gmail.com>
2020-03-26 19:13:13 -0700

    .

--- 8255_config/practica.c
+++ 8255_config/practica.c
@@ -17,6 +17,10 @@ void puts(char *str);
 char rotChar (char x, char n);
 void led1_out();
 void led2_out();
+void led3_out();
+void led4_out();
+void led5_out();
+void led6_out();
 char prueba2[50];
 char symbol[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 unsigned char salida[10];
@@ -25,16 +29,34 @@ int j,i;
 
 int main()
 {   
-    i=0;
+    int k;
+    int temp_dato;
+
+    outportb(RCtr,PTOs_all_in);
+    
     while(1)
     {
-        
+        for(k=0;k<8;k++)
+        {
             getch();
-            myItoa(TstBitPort(PC,4),10,salida);
-            puts(salida);
-            puts("alto");
+            temp_dato+=TstBitPort(PC,4);
+            temp_dato=temp_dato<<1;
+            puts("DATO_IN\n\r");
+        }
+
+        myItoa(temp_dato,2,salida);
+        puts(salida);
         led1_out();
+        getch();
         led2_out();
+        getch();
+        led3_out();
+        getch();
+        led4_out();
+        getch();
+        led5_out();
+        getch();
+        led6_out();
         getch();
     }
     return 0;
@@ -42,16 +64,44 @@ int main()
 
 void led1_out()
 {
-    outportb(RCtr,0X89);
+    outportb(RCtr,0x89);
     outportb(PA,0);
     outportb(PB,2);
 }
 
 void led2_out()
 {
-    outportb(RCtr,0X89);
+    outportb(RCtr,0x89);
     outportb(PA,2);
     outportb(PB,0);
+}
+
+void led3_out()
+{
+    outportb(RCtr,0x98);
+    outportb(PB,0);
+    outportb(PC,2);
+}
+
+void led4_out()
+{
+    outportb(RCtr,0x98);
+    outportb(PB,2);
+    outportb(PC,0);
+}
+
+void led5_out()
+{
+    outportb(RCtr,0x8A);
+    outportb(PA,2);
+    outportb(PC,0);
+}
+
+void led6_out()
+{
+    outportb(RCtr,0x8A);
+    outportb(PA,0);
+    outportb(PC,2);
 }
 
 void SetBitPort(WORD Puerto, BYTE num_bit)
